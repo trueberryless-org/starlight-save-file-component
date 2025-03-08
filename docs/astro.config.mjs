807e679c40af63d2da8ed748e7d5a41c6f5a89f2
trueberryless <trueberryless+github@gmail.com>
2025-03-09 00:26:11 +0100

    some changes

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -12,15 +12,8 @@ export default defineConfig({
       },
       sidebar: [
         {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
+          label: "Start here",
+          items: [{ slug: "getting-started" }, { slug: "properties" }],
         },
       ],
     }),
